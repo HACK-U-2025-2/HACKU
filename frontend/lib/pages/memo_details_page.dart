a4a88dd6377ee217e633ead846ae6e18bd64767a
tyPhoon <typhoonmoon1022@gmail.com>
2025-05-15 07:33:43 +0900

    feat: 基盤の追加

---
+++ frontend/lib/pages/memo_details_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MemoDetailsPage extends StatelessWidget {
+  const MemoDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('仮タイトル')),
+      body: const Placeholder(),
+    );
+  }
+}
---
+++ frontend/lib/pages/memo_list_view_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/pages/memo_details_page.dart';
+import 'package:frontend/widgets/memo_card.dart';
+
+class MemoListViewPage extends StatelessWidget {
+  const MemoListViewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('メモ一覧')),
+      body: ListView(
+        children: [
+          MemoCard(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute<void>(
+                  builder: (context) => const MemoDetailsPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
