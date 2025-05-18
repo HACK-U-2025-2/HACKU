43f2574ec0e38fcaaecb1ea024be7b33ea6ec139
tyPhoon <typhoonmoon1022@gmail.com>
2025-05-18 16:08:09 +0900

    fix: HomeDrawerDestinationの名前をプライベートに変更

--- frontend/lib/widgets/home_drawer.dart
+++ frontend/lib/widgets/home_drawer.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/pages/memo_list_view_page.dart';
 
-enum HomeDrawerDestination {
+enum _HomeDrawerDestination {
   home(iconData: Icons.home_outlined, label: 'ホーム'),
   memoList(iconData: Icons.list_outlined, label: 'メモ一覧'),
   tagList(iconData: Icons.label_outline, label: 'タグ一覧'),
   importantList(iconData: Icons.favorite_outline, label: '重要メモ'),
   archiveList(iconData: Icons.archive_outlined, label: 'アーカイブ');
 
-  const HomeDrawerDestination({required this.iconData, required this.label});
+  const _HomeDrawerDestination({required this.iconData, required this.label});
 
   final IconData iconData;
   final String label;
@@ -29,26 +29,26 @@ class HomeDrawer extends StatelessWidget {
             style: textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
           ),
         ),
-        for (final destination in HomeDrawerDestination.values)
+        for (final destination in _HomeDrawerDestination.values)
           NavigationDrawerDestination(
             icon: Icon(destination.iconData),
             label: Text(destination.label),
           ),
       ],
       onDestinationSelected: (value) {
-        final destination = HomeDrawerDestination.values[value];
+        final destination = _HomeDrawerDestination.values[value];
         switch (destination) {
-          case HomeDrawerDestination.home:
+          case _HomeDrawerDestination.home:
             Navigator.of(context).pop();
-          case HomeDrawerDestination.memoList:
+          case _HomeDrawerDestination.memoList:
             Navigator.of(context).push(
               MaterialPageRoute<void>(
                 builder: (context) => const MemoListViewPage(),
               ),
             );
-          case HomeDrawerDestination.tagList:
-          case HomeDrawerDestination.importantList:
-          case HomeDrawerDestination.archiveList:
+          case _HomeDrawerDestination.tagList:
+          case _HomeDrawerDestination.importantList:
+          case _HomeDrawerDestination.archiveList:
             // TODO: 各画面の実装
             showDialog<void>(
               context: context,
