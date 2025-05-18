4b7527f4211176713f79d7a7d5a2eff4e68df5be
Rozelin <rozelin.dc@gmail.com>
2025-05-18 11:26:36 +0900

    feat: メモカードの実装

--- frontend/lib/widgets/memo_card.dart
+++ frontend/lib/widgets/memo_card.dart
@@ -1,24 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:frontend/models/memo_preview.dart';
+import 'package:frontend/pages/memo_details_page.dart';
 
-class MemoCard extends StatelessWidget {
-  const MemoCard({super.key, this.onTap});
+class MemoCard extends HookWidget {
+  const MemoCard({required this.memoPreview, super.key, this.showBody = true});
 
-  final VoidCallback? onTap;
+  final MemoPreview memoPreview;
+  final bool showBody;
+
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    void onTap() {
+      Navigator.of(context).push(
+        MaterialPageRoute<void>(
+          // TODO(anyone): memoPreview.idを渡す, https://github.com/HACK-U-2025-2/HACKU/issues/22
+          builder: (context) => const MemoDetailsPage(),
+        ),
+      );
+    }
+
     return GestureDetector(
       onTap: onTap,
       child: Card(
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Padding(
-          padding: const EdgeInsets.all(8),
+          padding: const EdgeInsets.all(16),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 4,
             children: [
-              Text('仮タイトル', style: Theme.of(context).textTheme.bodyLarge),
-              const SizedBox(height: 4),
-              const Text('仮内容'),
+              Text(
+                memoPreview.title,
+                style: theme.textTheme.titleMedium?.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+              if (showBody)
+                Text(
+                  memoPreview.body,
+                  style: theme.textTheme.bodyMedium,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 3,
+                )
+              else
+                const SizedBox.shrink(),
             ],
           ),
         ),
