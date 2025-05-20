c6027dcd2dd63fd0c557e5f4ad0146a874dabbaf
tyPhoon <typhoonmoon1022@gmail.com>
2025-05-20 11:35:15 +0900

    feat: RecordDialogをリファクタリングし、ダイアログの表示と文字起こしの管理を改善

--- frontend/lib/widgets/dialogs/record_dialog.dart
+++ frontend/lib/widgets/dialogs/record_dialog.dart
@@ -4,10 +4,12 @@ import 'package:frontend/providers/recording_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:manual_speech_to_text/manual_speech_to_text.dart';
 
+/// 音声入力を行うダイアログを表示し、文字起こしされたテキストを返す
 Future<String?> pickTranscribed(BuildContext context, WidgetRef ref) async {
   final result = await showDialog<String?>(
     context: context,
-    builder: (context) => const RecordDialog(),
+    barrierDismissible: false, // 処理をシンプルにするために、画面外をタップしても閉じないようにする
+    builder: (context) => const _RecordDialog(),
   );
 
   if (result == null) {
@@ -17,12 +19,18 @@ Future<String?> pickTranscribed(BuildContext context, WidgetRef ref) async {
   return result;
 }
 
-class RecordDialog extends HookConsumerWidget {
-  const RecordDialog({super.key});
+class _RecordDialog extends HookConsumerWidget {
+  const _RecordDialog();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final transcribed = useState('');
+
+    // 今回使用しているSTTライブラリの録音の停止が非同期かつ、awaitできないため
+    // 録音の停止の管理をonListeningStateChangedで管理する
+    // そのとき、文字起こしを返すかどうかをフラグで管理する
+    final shouldPop = useState(false);
+    final shouldReturnTranscription = useState(false);
 
     final sttController = useMemoized(() {
       final sttController =
@@ -33,6 +41,13 @@ class RecordDialog extends HookConsumerWidget {
                   ref.read(isRecordingProvider.notifier).setAsRecording();
                 } else {
                   ref.read(isRecordingProvider.notifier).setAsNotRecording();
+                  if (shouldPop.value) {
+                    Navigator.of(context).pop(
+                      shouldReturnTranscription.value
+                          ? transcribed.value
+                          : null,
+                    );
+                  }
                 }
               },
               onListeningTextChanged: (recognizedText) {
@@ -60,6 +75,7 @@ class RecordDialog extends HookConsumerWidget {
     }, [sttController]);
 
     return AlertDialog(
+      icon: const Icon(Icons.mic),
       title: const Text('音声入力中'),
       content: Text(
         transcribed.value.isEmpty ? 'メモの内容を話してください' : transcribed.value,
@@ -68,14 +84,16 @@ class RecordDialog extends HookConsumerWidget {
         TextButton(
           onPressed: () {
             stop();
-            Navigator.of(context).pop();
+            shouldPop.value = true;
+            shouldReturnTranscription.value = false;
           },
           child: const Text('キャンセル'),
         ),
         TextButton(
           onPressed: () {
             stop();
-            Navigator.of(context).pop(transcribed.value);
+            shouldPop.value = true;
+            shouldReturnTranscription.value = true;
           },
           child: const Text('完了'),
         ),
