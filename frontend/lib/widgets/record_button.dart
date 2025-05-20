800438f61fd72ec0584fbee8a6617f6da5d30401
tyPhoon <typhoonmoon1022@gmail.com>
2025-05-20 10:34:39 +0900

    feat: manual_speech_to_textパッケージの統合と権限設定の追加

--- frontend/lib/widgets/record_button.dart
+++ frontend/lib/widgets/record_button.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:manual_speech_to_text/manual_speech_to_text.dart';
 
-class RecordButton extends StatelessWidget {
+class RecordButton extends HookWidget {
   const RecordButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final recordingState = useState(ManualSttState.stopped);
+    final transcribed = useState('');
+
+    final sttController = useMemoized(() {
+      final sttController =
+          ManualSttController(context)
+            ..listen(
+              onListeningStateChanged: (state) {
+                recordingState.value = state;
+              },
+              onListeningTextChanged: (recognizedText) {
+                debugPrint('Recognized text: $recognizedText');
+                transcribed.value = recognizedText;
+              },
+            )
+            ..enableHapticFeedback = true
+            ..localId = 'ja';
+
+      debugPrint('STT Controller initialized');
+      return sttController;
+    });
+    useEffect(() => sttController.dispose, [sttController]);
+
+    void start() {
+      sttController.startStt();
+    }
+
+    void stop() {
+      sttController.stopStt();
+    }
+
+    final onPressed =
+        recordingState.value == ManualSttState.listening ? stop : start;
+    final iconData =
+        recordingState.value == ManualSttState.listening
+            ? Icons.stop
+            : Icons.mic;
+
     return IconButton.filled(
       padding: const EdgeInsets.all(24),
-      onPressed: () {},
-      icon: const Icon(Icons.mic, size: 80),
+      onPressed: onPressed,
+      icon: Icon(iconData, size: 80),
     );
   }
 }
