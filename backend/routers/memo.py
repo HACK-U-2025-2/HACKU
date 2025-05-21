01bbf5e456224ee407ad936ba47024fbb54385ed
Mattya3 <m.tkm1217@ezweb.ne.jp>
2025-05-21 18:21:24 +0900

    feat: get memo

--- backend/routers/memo.py
+++ backend/routers/memo.py
@@ -1,18 +1,5 @@
 from typing import Annotated, List, Optional
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-from crud.auth import get_cuurent_user
-=======
-from crud.auth import get_current_user
->>>>>>> sprint1
-from crud.memo import fetch_memos
-from database import get_db
-from fastapi import APIRouter, Depends, Header, Query, status
-from schemas.auth import DecodedToken
-<<<<<<< HEAD
-from schemas.memo import MemoResponse
-=======
 from crud.auth import get_current_user
 from crud.memo import fetch_memo_by_id, fetch_memos
 from database import get_db
@@ -20,10 +7,6 @@ from fastapi import APIRouter, Depends, Header, HTTPException, Query, status
 from schemas.auth import DecodedToken
 from schemas.memo import MemoPreviewResponse, MemoResponse
 from schemas.tag import TagResponse
->>>>>>> Stashed changes
-=======
-from schemas.memo import MemoPreviewResponse
->>>>>>> sprint1
 from sqlalchemy.orm import Session
 from starlette import status
 
@@ -45,8 +28,7 @@ async def read_root(
     sort: Optional[str] = Query(None, description="ソート順(別issue)"),
 ):
     memos = fetch_memos(db=db, user_id=user.user_id, search_word=keyword)
-<<<<<<< HEAD
-    return [MemoPreviewResponse.model_validate(memo) for memo in memos]
+    return [MemoPreviewResponse.model_validate(m) for m in memos]
 
 
 @router.get("/{memo_id}", response_model=MemoResponse, status_code=status.HTTP_200_OK)
@@ -63,6 +45,3 @@ async def read_root(
     tag_response = [TagResponse.model_validate(memo_tag.tag) for memo_tag in memo.tags]
     memo_response = MemoResponse.model_validate({**memo.__dict__, "tags": tag_response})
     return memo_response
-=======
-    return [MemoPreviewResponse.model_validate(m) for m in memos]
->>>>>>> sprint1
