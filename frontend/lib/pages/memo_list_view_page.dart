4584a0046374f6522469a9b1154feefbb780670f
Rozelin <rozelin.dc@gmail.com>
2025-05-22 15:40:44 +0900

    feat: メモ追加導線のFAB実装

--- frontend/lib/pages/memo_list_view_page.dart
+++ frontend/lib/pages/memo_list_view_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:frontend/models/memo.dart';
 import 'package:frontend/models/memo_preview.dart';
 import 'package:frontend/widgets/destination_navigation_drawer.dart';
+import 'package:frontend/widgets/dialogs/record_dialog.dart';
 import 'package:frontend/widgets/memo_card.dart';
 
 class MemoListViewPage extends StatelessWidget {
@@ -22,13 +24,103 @@ class MemoListViewPage extends StatelessWidget {
     return Scaffold(
       drawer: const DestinationNavigationDrawer(),
       appBar: AppBar(title: const Text('メモ一覧')),
+      floatingActionButton: const _AddMemoFab(),
       body: ListView(
+        // TODO(Rozelin-dc): FABに被らないようにpaddingを調整
         children: [
           ...mockMemoList.map(
             (memoPreview) => MemoCard(memoPreview: memoPreview),
           ),
         ],
       ),
+    );
+  }
+}
+
+class _AddMemoFab extends HookWidget {
+  const _AddMemoFab();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final isOpen = useState(false);
+
+    const animationDuration = Duration(milliseconds: 300);
+    final animationController = useAnimationController(
+      duration: animationDuration,
+    );
+    final slideAnimation = CurvedAnimation(
+      parent: animationController,
+      curve: Curves.easeOut,
+    );
+    final editButtonAnimation = Tween<Offset>(
+      begin: const Offset(0, 2.5),
+      end: Offset.zero,
+    ).animate(slideAnimation);
+    final micButtonAnimation = Tween<Offset>(
+      begin: const Offset(0, 1.5),
+      end: Offset.zero,
+    ).animate(slideAnimation);
+    useEffect(() {
+      if (isOpen.value) {
+        animationController.forward();
+      } else {
+        animationController.reverse();
+      }
+      return null;
+    }, [isOpen.value]);
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SlideTransition(
+          position: editButtonAnimation,
+          child: FloatingActionButton(
+            heroTag: 'add-memo-text-fab',
+            mini: true,
+            onPressed: () {
+              // TODO(Rozelin-dc): メモ追加のダイアログ表示
+              debugPrint('メモ追加');
+            },
+            child: const Icon(Icons.edit),
+          ),
+        ),
+        const SizedBox(height: 5),
+        SlideTransition(
+          position: micButtonAnimation,
+          child: FloatingActionButton(
+            heroTag: 'add-memo-voice-fab',
+            mini: true,
+            onPressed: () async {
+              final transcription = await pickTranscribed(context);
+              if (transcription == null) return;
+
+              // TODO(Rozelin-dc): メモ追加処理
+              debugPrint('音声メモ追加: $transcription');
+            },
+            child: const Icon(Icons.mic),
+          ),
+        ),
+
+        const SizedBox(height: 10),
+
+        FloatingActionButton(
+          backgroundColor:
+              isOpen.value
+                  ? theme.colorScheme.secondaryContainer
+                  : theme.colorScheme.primary,
+          foregroundColor:
+              isOpen.value
+                  ? theme.colorScheme.onSecondaryContainer
+                  : theme.colorScheme.onPrimary,
+          onPressed: () => isOpen.value = !isOpen.value,
+          child: RotationTransition(
+            turns: animationController,
+            child: Icon(isOpen.value ? Icons.close : Icons.add),
+          ),
+        ),
+      ],
     );
   }
 }
