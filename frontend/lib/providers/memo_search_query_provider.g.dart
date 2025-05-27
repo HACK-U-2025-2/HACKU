451692bdf9c7580386a3ae6dc45c2dd15fb5e892
Rozelin <rozelin.dc@gmail.com>
2025-05-27 11:36:20 +0900

    feat: 一覧取得用のproviderの用意

---
+++ frontend/lib/providers/memo_search_query_provider.g.dart
@@ -0,0 +1,63 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'memo_search_query_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$memoSearchKeywordHash() => r'f15b47884229840884852c7c943027a6b351e228';
+
+/// See also [MemoSearchKeyword].
+@ProviderFor(MemoSearchKeyword)
+final memoSearchKeywordProvider =
+    AutoDisposeNotifierProvider<MemoSearchKeyword, String>.internal(
+      MemoSearchKeyword.new,
+      name: r'memoSearchKeywordProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$memoSearchKeywordHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$MemoSearchKeyword = AutoDisposeNotifier<String>;
+String _$memoSearchTagNamesHash() =>
+    r'207d5ea82a956aeb2378a78617ffd275e765188b';
+
+/// See also [MemoSearchTagNames].
+@ProviderFor(MemoSearchTagNames)
+final memoSearchTagNamesProvider =
+    AutoDisposeNotifierProvider<MemoSearchTagNames, List<String>>.internal(
+      MemoSearchTagNames.new,
+      name: r'memoSearchTagNamesProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$memoSearchTagNamesHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$MemoSearchTagNames = AutoDisposeNotifier<List<String>>;
+String _$memoSearchSortOptionHash() =>
+    r'd8551dc930bfbc63bce94c9fd0c99fca7ef26a88';
+
+/// See also [MemoSearchSortOption].
+@ProviderFor(MemoSearchSortOption)
+final memoSearchSortOptionProvider =
+    AutoDisposeNotifierProvider<MemoSearchSortOption, MemoSortOption?>.internal(
+      MemoSearchSortOption.new,
+      name: r'memoSearchSortOptionProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$memoSearchSortOptionHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$MemoSearchSortOption = AutoDisposeNotifier<MemoSortOption?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
