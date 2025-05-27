2bf2bebf3e11d9fe2ffc6d408ef4c064ff6f6abc
ootake430 <ootake430@gmail.com>
2025-05-27 21:36:29 +0900

    chore: フォーマット修正

--- backend/main.py
+++ backend/main.py
@@ -44,19 +44,3 @@ app.include_router(auth.router)
 app.include_router(tag.router)
 app.include_router(memo.router)
 app.include_router(memo_websocket.router)
-
-if __name__ == "__main__":
-    from llm.summarize_text import summarize_text
-
-    print("-" * 10, "summarize_text", "-" * 10)
-    print(
-        summarize_text(
-            "夜の村で何か起きるみたいな感じのやつでも全部がそうじゃなくてなんか選ばれた人だけっていうか理由はまだよくわかんないけど昔の話とか関係あるかもで少年がその中に入ってうーん"
-        )
-    )
-
-    from embedding.embedding import get_embedding
-    from embedding.reduce_to_3d import embedding_to_3d_unit
-
-    x = get_embedding("夜の村で何か起きるみたいな感じ")
-    print(embedding_to_3d_unit(x))
