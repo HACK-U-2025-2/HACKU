3a49ec3ff97184f623a3b1bd0af1b4e8963c170a
tyPhoon <typhoonmoon1022@gmail.com>
2025-05-28 12:26:10 +0900

    feat: SpherePageの初期状態をWeb環境に応じて設定

--- frontend/lib/pages/sphere_page.dart
+++ frontend/lib/pages/sphere_page.dart
@@ -1,6 +1,7 @@
 import 'dart:math' as math;
 
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:frontend/pages/errors/sphere_error_page.dart';
@@ -18,7 +19,9 @@ class SpherePage extends HookConsumerWidget {
 
     final controller = useMemoized(GraphifyController.new);
     final wireframe = useMemoized(generateEChartsWireframeData);
-    final isSphereLoading = useState(true);
+    final isSphereLoading = useState(
+      !kIsWeb,
+    ); // Webではライブラリの都合でonCreatedが呼ばれないため、初期状態でtrueにしている
 
     useEffect(() {
       return controller.dispose;
