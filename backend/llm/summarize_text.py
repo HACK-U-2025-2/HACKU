f5e82fa3bf7c2cb1f35fb26e4c5501fa39069ae0
Mattya3 <m.tkm1217@ezweb.ne.jp>
2025-05-28 23:50:24 +0900

    fix: llm

--- backend/llm/summarize_text.py
+++ backend/llm/summarize_text.py
@@ -5,11 +5,10 @@ from llm.utils.parser import parse_json
 def summarize_text(text: str) -> str:
     prompt = f"""
 # 指示
-以下の文章を簡潔に要約し、段落つきの箇条書き等を使用し、スライドで発表しやすい形になるようMarkdown形式で出力してください。
+以下の文章を簡潔に箇条書きで要約し、Markdown形式で出力してください。
 
 # 文章
 {text}
 """
     json_text = generate_text(prompt, enable_thinking=False, max_new_tokens=512)
-    print(json_text)
     return parse_json(json_text)
