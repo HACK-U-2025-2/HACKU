4c9ad89495da3f1121e17039cc546a8eda23a057
Rozelin <rozelin.dc@gmail.com>
2025-05-29 12:32:41 +0900

    feat: メモ詳細からドロワーが表示される画面まで一気に戻れるように

---
+++ frontend/lib/widgets/custom_back_button.dart
@@ -0,0 +1,19 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/router.dart';
+
+/// ドロワーが表示される画面まで一気に戻るボタン
+class CustomBackButton extends StatelessWidget {
+  const CustomBackButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BackButton(
+      onPressed: () {
+        if (!context.mounted) return;
+
+        context.router.popUntil((route) => route.data?.toDestination() != null);
+      },
+    );
+  }
+}
