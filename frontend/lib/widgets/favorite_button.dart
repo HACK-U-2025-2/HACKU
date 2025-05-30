10a4c2006d5e582cca1b14ce01c39dddc721361f
Rozelin <rozelin.dc@gmail.com>
2025-05-30 15:31:50 +0900

    feat: お気に入り更新でエラーハンドリングをするように

--- frontend/lib/widgets/favorite_button.dart
+++ frontend/lib/widgets/favorite_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/models/memo.dart';
 import 'package:frontend/providers/repository_provider.dart';
+import 'package:frontend/types/extensions/snack_bar.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class FavoriteButton extends ConsumerWidget {
@@ -19,12 +20,20 @@ class FavoriteButton extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return IconButton(
       icon: Icon(isFavorite ? Icons.lock : Icons.lock_open),
-      onPressed: () {
-        ref
-            .read(memoRepositoryProvider)
-            .updateMemoFavorite(memoId, isFavorite: !isFavorite);
-
-        afterToggle?.call();
+      onPressed: () async {
+        try {
+          await ref
+              .read(memoRepositoryProvider)
+              .updateMemoFavorite(memoId, isFavorite: !isFavorite);
+          afterToggle?.call();
+        } on Exception catch (e) {
+          debugPrint('Error update memo.isFavorite: $e');
+          if (context.mounted) {
+            ScaffoldMessenger.of(
+              context,
+            ).showErrorSnackBar(message: 'Myndのロック設定更新に失敗しました。やり直してください');
+          }
+        }
       },
     );
   }
